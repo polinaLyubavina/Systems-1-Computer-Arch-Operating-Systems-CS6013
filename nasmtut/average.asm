6bb13c9c1dfcfe010a834abc22a0633c9864e077
polinaLyubavina <polinalyub@tutanota.com>
2022-01-29 15:45:58 -0700

    add average.asm

--- nasmtut/average.asm
+++ nasmtut/average.asm
@@ -3,15 +3,21 @@
 ; displays their average as a floating point number.  This program uses a data
 ; section to store intermediate results, not that it has to, but only to
 ; illustrate how data sections are used.
+;
+; Designed for OS X.  To assemble and run:
+;
+;     nasm -fmacho64 average.asm && gcc average.o && ./a.out
 ; -----------------------------------------------------------------------------
 
-        global   main
-        extern   atoi
-        extern   printf
+        global   _main
+        extern   _atoi
+        extern   _printf
         default  rel
 
         section  .text
-main:
+_main:
+        push     rbx                    ; we don't ever use this, but it is necesary
+                                        ; to align the stack so we can call stuff
         dec      rdi                    ; argc-1, since we don't count program name
         jz       nothingToAverage
         mov      [count], rdi           ; save number of real arguments
@@ -19,7 +25,7 @@ accumulate:
         push     rdi                    ; save register across call to atoi
         push     rsi
         mov      rdi, [rsi+rdi*8]       ; argv[rdi]
-        call     atoi                   ; now rax has the int value of arg
+        call     _atoi                  ; now rax has the int value of arg
         pop      rsi                    ; restore registers after atoi call
         pop      rdi
         add      [sum], rax             ; accumulate sum as we go
@@ -29,19 +35,18 @@ average:
         cvtsi2sd xmm0, [sum]
         cvtsi2sd xmm1, [count]
         divsd    xmm0, xmm1             ; xmm0 is sum/count
-        mov      rdi, format            ; 1st arg to printf
+        lea      rdi, [format]          ; 1st arg to printf
         mov      rax, 1                 ; printf is varargs, there is 1 non-int argument
-
-        sub      rsp, 8                 ; align stack pointer
-        call     printf                 ; printf(format, sum/count)
-        add      rsp, 8                 ; restore stack pointer
-
-        ret
+        call     _printf                ; printf(format, sum/count)
+        jmp      done
 
 nothingToAverage:
-        mov      rdi, error
+        lea      rdi, [error]
         xor      rax, rax
-        call     printf
+        call     _printf
+
+done:
+        pop      rbx                    ; undoes the stupid push at the beginning
         ret
 
         section  .data
